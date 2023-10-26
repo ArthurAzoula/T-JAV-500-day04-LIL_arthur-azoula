d43d0892537a18dd503b7536363b6c4f017275f3
Arthur Azoula <arthur.azoula@epitech.eu>
2023-10-26 11:53:32 +0200

    Edit tasK05

--- Shark.java
+++ Shark.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Shark extends Animal {
 
     private boolean frenzy = false;
@@ -15,6 +17,19 @@ public class Shark extends Animal {
             System.out.println(this.getName() + " is smelling blood and wants to kill.");
         } else {
             System.out.println(this.getName() + " is swimming peacefully.");
+        }
+    }
+
+    public boolean canEat(Animal animal) {
+        return this != animal;
+    }
+
+    public void eat(Animal animal) {
+        if (this.canEat(animal)) {
+            this.frenzy = false;
+            System.out.println(this.getName() + " ate a " + animal.getType() + " named " + animal.getName() + ".");
+        } else {
+            System.out.println(this.getName() + " It's not worth my time.");
         }
     }
 
