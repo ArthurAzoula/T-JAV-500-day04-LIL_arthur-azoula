75d0fbbdff02a37276339c36548b7d3834007ce4
Arthur Azoula <arthur.azoula@epitech.eu>
2023-10-27 11:24:51 +0200

    edit day04

--- GreatWhite.java
+++ GreatWhite.java
@@ -6,17 +6,22 @@ public class GreatWhite extends Shark {
 
     @Override
     public boolean canEat(Animal animal) {
-        return !(animal instanceof Canary) && super.canEat(animal);
+        return super.canEat(animal) && !(animal instanceof Canary);
     }
 
     @Override
     public void eat(Animal animal) {
-        if (animal instanceof Canary) {
-            System.out.println(this.name + ": Next time you try to give me that to eat, I'll eat you instead.");
-        } else if (animal instanceof Shark && super.canEat(animal)) {
-            System.out.println(this.name + ": The best meal one could wish for.");
+        if (canEat(animal)) {
+            System.out.println(getName() + " ate a " + animal.getType() + " named " + animal.getName() + ".");
+            smellBlood(false);
+
+            if (animal instanceof Shark) {
+                System.out.println(getName() + ": The best meal one could wish for.");
+            }
+        } else if (animal instanceof Canary) {
+            System.out.println(getName() + ": Next time you try to give me that to eat, I'll eat you instead.");
         } else {
-            super.eat(animal);
+            System.out.println(getName() + ": It's not worth my time.");
         }
 
     }
